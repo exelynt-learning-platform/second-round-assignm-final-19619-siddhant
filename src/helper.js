cc01b113432a85e20443ea6154cd7a0dd2487cfb
Siddhant Shinde <149939788+Siddhant0958@users.noreply.github.com>
2026-04-03 01:30:43 +0530

    Delete src directory

--- src/helper.js
+++
@@ -1,9 +0,0 @@
-export function checkHeading(str) {
-  if (!str) return false;
-  return /^\*\*(.*?)\*\*$/.test(str.trim());
-}
-
-export function replaceHeadingStarts(str) {
-  if (!str) return "";
-  return str.trim().replace(/^\*\*|\*\*$/g, "");
-}
