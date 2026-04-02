6d790f22506f09d563efc8bf1dde0d300834d4dc
Siddhant Shinde <149939788+Siddhant0958@users.noreply.github.com>
2026-04-02 20:51:46 +0530

    Update answers.jsx

--- src/components/answers.jsx
+++ src/components/answers.jsx
@@ -1,34 +1,26 @@
-import { useEffect, useState } from "react";
 import { checkHeading, replaceHeadingStarts } from "../helper";
 
+const Answer = ({ ans, totalResult, index, type }) => {
+  const isHeading = checkHeading(ans);
+  const finalAnswer = isHeading ? replaceHeadingStarts(ans) : ans;
 
-const Answer=({ans,totalResult, index,type})=>{
+  return (
+    <>
+      {totalResult > 1 ? (
+        <span className="pt-2 text-lg block text-white">
+          {finalAnswer}
+        </span>
+      ) : isHeading ? (
+        <span className="pt-2 text-lg block text-white">
+          {finalAnswer}
+        </span>
+      ) : (
+        <span className={type === "q" ? "pl-1" : "pl-5"}>
+          {finalAnswer}
+        </span>
+      )}
+    </>
+  );
+};
 
-    const [heading,setHeading]=useState(false);
-    const [answer,setAnswer]=useState(ans);
-    // console.log(index);
-
-
-    useEffect(()=>{
-        if(checkHeading(ans)){
-        setHeading(true);
-        setAnswer(replaceHeadingStarts(ans))
-
-}
-
-},[])
-
-
-    return(
-        <>
-        {
-            index && totalResult>1?<span className="pt-2 text-lg block text-white">{answer} </span>:
-            heading?<span className={"pt-2 text-lg block text-white"}>{answer}</span>
-            :<span className={type=='q'?'pl-1':'pl-5'}>{answer}</span>
-        }
-
-        </> 
-    )
-}
-
-export default Answer
+export default Answer;
