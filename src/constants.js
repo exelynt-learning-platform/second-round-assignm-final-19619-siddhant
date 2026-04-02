c6f321b5926490fd8fafe64efccd3f5bca3375e1
Siddhant Shinde <149939788+Siddhant0958@users.noreply.github.com>
2026-04-02 20:14:37 +0530

    Add files via upload

---
+++ src/constants.js
@@ -0,0 +1,21 @@
+export const URL="https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=AIzaSyCwOTyjCc2iNYZyQXGe5la4ZLwD_d4ISKg"
+
+
+// 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"
+// https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent?key=
+ 
+// curl "https://generativelanguage.googleapis.com/v1beta/models/gemini-flash-latest:generateContent" \
+//   -H 'Content-Type: application/json' \
+//   -H 'X-goog-api-key: AIzaSyCGDNJpvrGj2Qi3wRUW917JLAedYKTcIf0' \
+//   -X POST \
+//   -d '{
+//     "contents": [
+//       {
+//         "parts": [
+//           {
+//             "text": "Explain how AI works in a few words"
+//           }
+//         ]
+//       }
+//     ]
+//   }'
